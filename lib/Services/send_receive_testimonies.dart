f75a9b845d433a226cc52a45b85d6420ee157f76
henryadebayo <henryadebayo250@gmail.com>
2022-01-15 20:08:45 +0100

    connecting to firebase

--- lib/Services/send_receive_testimonies.dart
+++ lib/Services/send_receive_testimonies.dart
@@ -1,9 +1,13 @@
 import 'dart:convert';
-import 'dart:html';
 
 import 'package:flutter/material.dart';
 import 'package:stea/models/testimonyModel.dart';
 import 'package:http/http.dart'as http;
+
+List<TestimonyModel> _testimonis = [];
+List<TestimonyModel> get testimonis {
+  return List.from(_testimonis);
+}
 
 String url = "https://stea-880c0-default-rtdb.firebaseio.com/testimonies.json";
 
@@ -28,16 +32,35 @@ print(e);
   // Receiveing testimonies from database 
 
 Future<TestimonyModel> fetchTestimonies()async{
+  //int testimonyIndex = testimonis
+try {
   final http.Response response = await http.get(Uri.parse(url));
   final Map<String, dynamic> responseData = json.decode(response.body);
 
-  responseData.forEach((String id, dynamic tdata){
+  responseData.forEach((String id, dynamic tdata) {
+    //TestimonyModel theTestimony = getFoodTestimonyWithId(tdata);
+
     TestimonyModel testimonies = TestimonyModel(
       id: id,
       name: tdata["name"],
       details: tdata["details"],
     );
+    _testimonis.add(testimonies);
   });
-
+}catch (e){
+  print(e);
+}
   return fetchTestimonies();
+
+}
+
+TestimonyModel getFoodTestimonyWithId(String tdata) {
+  TestimonyModel testimonyModel;
+  for(int i = 0; i< _testimonis.length; i++){
+    if(_testimonis[i].id == tdata){
+      testimonyModel = _testimonis[i];
+      break;
+    }
+    return testimonyModel;
+  }
 }
