db805f3fadc570132b23c0f6568a6eef351db050
henryadebayo <henryadebayo250@gmail.com>
2022-01-17 04:03:07 +0100

    connecting to firebase

---
+++ lib/scopedModel/mainModel.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:stea/Services/send_receive_testimonies.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:stea/Services/send_receive_testimonies.dart';
+
+class MainModel extends Model{
+}
