4fca6f8f9440204b5018f3dbe919d42b74026ab9
henryadebayo <henryadebayo250@gmail.com>
2022-01-21 00:03:37 +0100

    fixing scoped model bug and services bug

--- lib/widgets/testimonyItem.dart
+++ lib/widgets/testimonyItem.dart
@@ -9,43 +9,34 @@ class testimonyItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 220,
-      width: double.infinity,
+      height: 220.h,
+       width: double.infinity,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        image: DecorationImage(
-            image: AssetImage("images/icon1.png"), fit: BoxFit.cover),
+        image: DecorationImage(image: AssetImage("images/beautiful-bright-blue-bloom-petals-dew.jpg"),
+          fit: BoxFit.cover,
+        ),
+        borderRadius: BorderRadius.circular(20.0.r),
       ),
-      child: Container(
-        height: 100.h,
-        width: 180.w,
-        decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage("images/beautiful-bright-blue-bloom-petals-dew.jpg"),
-            fit: BoxFit.cover,
+      child: Column(
+        children: [
+          SizedBox(height: 10.0),
+          Text(
+            testifyerName,
+            style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w900,
+                fontSize: 20.0),
           ),
-          borderRadius: BorderRadius.circular(30.0.r),
-        ),
-        child: Column(
-          children: [
-            SizedBox(height: 10.0),
-            Text(
-              testifyerName,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w900,
-                  fontSize: 20.0),
-            ),
-            Padding(
-                padding: EdgeInsets.only(left: 10.0, right: 5.0, top: 8.0),
-                child: Text(
-                  testifyerText,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18.0,
-                      fontStyle: FontStyle.italic),
-                )),
-          ],
-        ),
+          Padding(
+              padding: EdgeInsets.only(left: 10.0, right: 5.0, top: 8.0),
+              child: Text(
+                testifyerText,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                    fontStyle: FontStyle.italic),
+              )),
+        ],
       ),
     );
   }
