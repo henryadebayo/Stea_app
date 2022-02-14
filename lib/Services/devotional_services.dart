08edbef06b1b6851b5f2ea09fe165dd553de025a
henryadebayo <henryadebayo250@gmail.com>
2022-02-14 03:11:10 +0100

    Building Devotionals screen and Widgets

---
+++ lib/Services/devotional_services.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+
+class SteaDevotionals {
+  
+  static Future<File> loadDevotional(String url)async {
+    final refPDF = FirebaseStorage.instance.ref().child(url);
+    final byte = await refPDF.getData();
+    return _storeFile(url, byte);
+  }
+  
+  
+}
