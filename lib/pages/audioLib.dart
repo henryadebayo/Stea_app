6872249749ec7999a1c688f1049a6358fb18df06
henryadebayo <henryadebayo250@gmail.com>
2022-02-14 13:47:09 +0100

    Building Done building audiolib Widgets

--- lib/pages/audioLib.dart
+++ lib/pages/audioLib.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter/material.dart';
+import 'package:stea/widgets/audioLibCard.dart';
 import 'package:stea/widgets/bottomNavigation.dart';
+import 'package:stea/widgets/const.dart';
+import 'package:stea/widgets/testimonyItem.dart';
 
 class AudioLib extends StatefulWidget {
   const AudioLib({Key key}) : super(key: key);
@@ -13,7 +16,19 @@ class _AudioLibState extends State<AudioLib> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     // bottomNavigationBar: BottomNavigation(),
+     appBar: AppBar(
+       title: Text("Audio Sermons"),
+       backgroundColor: KdarkBlueColour,
+       centerTitle: true,
+     ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8.0),
+        child: ListView.builder(
+          itemCount: 20,
+            itemBuilder: (BuildContext context, int index){
+          return AudioLibCard();
+        }),
+      )
     );
   }
 }
--- lib/widgets/audioLibCard.dart
+++ lib/widgets/audioLibCard.dart
@@ -1,9 +1,70 @@
-import 'package:flutter/material.dart';
-class AudioLibCard extends StatelessWidget {
-  const AudioLibCard({Key key}) : super(key: key);
+import 'dart:ui';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+class AudioLibCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding:EdgeInsets.symmetric(vertical:3.0.h),
+      child: Container(
+        height: 170.0.h,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage("images/beautiful-bright-blue-bloom-petals-dew.jpg"), fit:  BoxFit.cover),
+         // borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: ClipRRect( // make sure we apply clip it properly
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 8, sigmaY: 5),
+            child: Container(
+              height: 170.0.h,
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.1),
+                borderRadius: BorderRadius.circular(20.0.r),
+              ),
+              //alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                //crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding:  EdgeInsets.only(top:8.0.h, left: 8.0.w),
+                    child: Text(
+                      "Ecounter with the Holy spirit",
+                      style: TextStyle(fontSize: 20.0.sp, color: Colors.white),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 8.0.w),
+                    child: Text("dghafmabdkjcJSZ c,kjc ldsvbkgcK>BDlk:HILDf.ndvldhvcujBN><DAfc LKhkhdkahfkuhakd akfhakjsnf,andlflkahdljfknaldkfhcuadhkfkjankn.......",
+                      style: TextStyle(color: Colors.white),),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 16.0.h),
+                    child: Row(
+                      //crossAxisAlignment: Cross,
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        IconButton(
+                          onPressed: (){},
+                            icon:
+                            Icon(Icons.download_outlined, color: Colors.white,size: 40.0,)
+                        ),
+                        IconButton(
+                          onPressed: (){},
+                            icon:
+                            Icon(Icons.play_circle_fill, color: Colors.white,size: 40.0,)
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
