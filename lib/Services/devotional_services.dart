ef48196dcfc9cf1f80600626dd101a49f2c11013
henryadebayo <henryadebayo250@gmail.com>
2022-02-15 03:51:49 +0100

    Building devotional functionality

--- lib/Services/devotional_services.dart
+++ lib/Services/devotional_services.dart
@@ -1,15 +1,33 @@
+import 'dart:html';
 import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 class SteaDevotionals {
-  
-  static Future<File> loadDevotional(String url)async {
-    final refPDF = FirebaseStorage.instance.ref().child(url);
-    final byte = await refPDF.getData();
-   // return _storeFile(url, byte);
+
+FilePicker filePicker;
+
+Future getPdfUpload()async{
+  File file = (await filePicker.pickFiles(type: FileType.custom)) as File;
+  //String fileName = "$"
+  savePdf(file.readAsBytes());
+}
+
+  void savePdf(Future<Uint8List> asset)async {
+  StorageReference reference = FirebaseStorage.instance.ref().child(path);
+  StorageUploadTask uploadTask = reference.putData(asset);
+  String url = await (await uploadTask.onComplete).ref.getDownloadURL();
+  documentFileUpload(url);
   }
-  
+
+  // static Future<File> loadDevotional(String url)async {
+  //   final refPDF = FirebaseStorage.instance.ref().child(url);
+  //   final byte = await refPDF.getData();
+  //  // return _storeFile(url, byte);
+  // }
+  //
   
 }
--- lib/models/devotionalModel.dart
+++ lib/models/devotionalModel.dart
@@ -1,12 +1,21 @@
-class devotinal {
+class devotional {
+  final String id;
   final String devotionalName;
   final String devotionalDescription;
   final String devotionalPath;
 
-  devotinal({
+  devotional({
+    this.id,
     this.devotionalName,
     this.devotionalDescription,
     this.devotionalPath,
 });
+ factory devotional.fromJson(Map<String, dynamic> json)=>
+   devotional(
+     id: json['id'],
+     devotionalName: json["name"],
+     devotionalDescription: json['description'],
+     devotionalPath: json['pdfPath'],
+   );
 
 }
