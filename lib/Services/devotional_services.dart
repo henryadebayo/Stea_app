116a6d72742aa8a34d38de12976784a97e9dcf4b
henryadebayo <henryadebayo250@gmail.com>
2022-02-15 21:33:02 +0100

    Building devotional functionality

--- lib/Services/devotional_services.dart
+++ lib/Services/devotional_services.dart
@@ -1,14 +1,15 @@
-import 'dart:html';
 import 'dart:io';
-import 'dart:typed_data';
+//import 'dart:typed_data';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:stea/models/devotionalModel.dart';
 
-class SteaDevotionals {
+class SteaDevotionals extends ChangeNotifier{
 
 FilePicker filePicker;
+DevotionalModel devotional = DevotionalModel();
 
 Future getPdfUpload()async{
   File file = (await filePicker.pickFiles(type: FileType.custom)) as File;
@@ -17,12 +18,27 @@ Future getPdfUpload()async{
 }
 
   void savePdf(Future<Uint8List> asset)async {
-  StorageReference reference = FirebaseStorage.instance.ref().child(path);
-  StorageUploadTask uploadTask = reference.putData(asset);
-  String url = await (await uploadTask.onComplete).ref.getDownloadURL();
+  Reference ref = FirebaseStorage.instance.ref().child(path);
+ UploadTask upload = ref.putData(asset);
+ String url;
+  upload.whenComplete(() {
+    url = ref.getDownloadURL() as String;
+  }).catchError((onError) {
+    print(onError);
+  });
+  //return url;
+  // String url = await (await upload.whenComplete(() => null)).ref.getDownloadURL();
   documentFileUpload(url);
   }
+void documentFileUpload(String url) {
+var devotionalModel = {
+  "devtionalName": devotional.devotionalName,
+  "devotionalPath": devotional.devotionalPath,
+  "devotionalDescription": devotional.devotionalDescription,
+  "devotionalId": devotional.id,
+};
 
+}
   // static Future<File> loadDevotional(String url)async {
   //   final refPDF = FirebaseStorage.instance.ref().child(url);
   //   final byte = await refPDF.getData();
--- lib/models/devotionalModel.dart
+++ lib/models/devotionalModel.dart
@@ -1,17 +1,17 @@
-class devotional {
+class DevotionalModel {
   final String id;
   final String devotionalName;
   final String devotionalDescription;
   final String devotionalPath;
 
-  devotional({
+  DevotionalModel({
     this.id,
     this.devotionalName,
     this.devotionalDescription,
     this.devotionalPath,
 });
- factory devotional.fromJson(Map<String, dynamic> json)=>
-   devotional(
+ factory DevotionalModel.fromJson(Map<String, dynamic> json)=>
+   DevotionalModel(
      id: json['id'],
      devotionalName: json["name"],
      devotionalDescription: json['description'],
